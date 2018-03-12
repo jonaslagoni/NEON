ddb2a35739bb0982b50a9f58e9c5bce924186062
jonaslagoni <jonas-lt@live.dk>
2018-03-12 11:05:02 +0100

    Added pathfinding

---
+++ src/main/java/com/neon/common/search/Step.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neon.common.search;
+
+
+/**
+ * A single step within the path
+ * 
+ * @author Kevin Glass
+ */
+public class Step {
+        /** The x coordinate at the given step */
+        protected int x;
+        /** The y coordinate at the given step */
+        protected int y;
+
+        /**
+         * Create a new step
+         * 
+         * @param x The x coordinate of the new step
+         * @param y The y coordinate of the new step
+         */
+        public Step(int x, int y) {
+                this.x = x;
+                this.y = y;
+        }
+
+        /**
+         * Get the x coordinate of the new step
+         * 
+         * @return The x coodindate of the new step
+         */
+        public int getX() {
+                return x;
+        }
+
+        /**
+         * Get the y coordinate of the new step
+         * 
+         * @return The y coodindate of the new step
+         */
+        public int getY() {
+                return y;
+        }
+
+        /**
+         * @see Object#hashCode()
+         */
+        public int hashCode() {
+                return x*y;
+        }
+
+        /**
+         * @see Object#equals(Object)
+         */
+        public boolean equals(Object other) {
+                if (other instanceof Step) {
+                        Step o = (Step) other;
+
+                        return (o.x == x) && (o.y == y);
+                }
+
+                return false;
+        }
+}
---
+++ src/main/java/com/neon/search/astar/Node.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.neon.search.astar;
+
+/**
+ * A single node in the search graph
+ */
+public class Node implements Comparable {
+        /** The x coordinate of the node */
+        protected int x;
+        /** The y coordinate of the node */
+        protected int y;
+        /** The path cost for this node */
+        protected float cost;
+        /** The parent of this node, how we reached it in the search */
+        protected Node parent;
+        /** The heuristic cost of this node */
+        protected float heuristic;
+        /** The search depth of this node */
+        protected int depth;
+
+        /**
+         * Create a new node
+         * 
+         * @param x The x coordinate of the node
+         * @param y The y coordinate of the node
+         */
+        public Node(int x, int y) {
+                this.x = x;
+                this.y = y;
+        }
+
+        /**
+         * Set the parent of this node
+         * 
+         * @param parent The parent node which lead us to this node
+         * @return The depth we have now reached in searching
+         */
+        public int setParent(Node parent) {
+                depth = parent.depth + 1;
+                this.parent = parent;
+
+                return depth;
+        }
+
+        /**
+         * @see Comparable#compareTo(Object)
+         */
+        @Override
+        public int compareTo(Object other) {
+                Node o = (Node) other;
+
+                float f = heuristic + cost;
+                float of = o.heuristic + o.cost;
+
+                if (f < of) {
+                        return -1;
+                } else if (f > of) {
+                        return 1;
+                } else {
+                        return 0;
+                }
+        }
+}
