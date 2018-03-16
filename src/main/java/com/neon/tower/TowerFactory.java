ff9feec1056ea45f23b1cea8ac80a0b0bd1a8619
Emil Tang Kristensen <emilkristensen@protonmail.com>
2018-03-16 14:42:29 +0100

    Fix missing parameters in tower constructor

--- src/main/java/com/neon/tower/TowerFactory.java
+++ src/main/java/com/neon/tower/TowerFactory.java
@@ -4,158 +4,76 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.neon.libary.Sprite;
 import com.neon.libary.World;
-import com.neon.libary.interfaces.Entity;
-import com.neon.libary.interfaces.Factory;
 import com.neon.libary.vectors.Vector2f;
 import com.neon.weapon.Weapon;
 
 public class TowerFactory {
 
-    
     public static Tower build(String key) {
         switch (key) {
             case "laser-tower":
-                Sprite laserSprite = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 1.png",
+                        "images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 2.png",
+                        "images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 3.png",
+                        "images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 4.png"
                 );
-
-                Texture[] laserUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 2.png")),
-                        new Texture(Gdx.files.internal("images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 3.png")),
-                        new Texture(Gdx.files.internal("images/towers/Laser Tower (Constant focused single target)/Laser Tower lvl 4.png"))
-                };
-
-                Weapon laserWeapon = new Weapon(laserSprite.getPosition(), 512);
-                return new Tower(laserSprite, laserUpgrades, laserWeapon, 10, 4);
-            
             case "melee-glaive-tower":
-                Sprite meleeTower = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 1.1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 1.1.png",
+                        "images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 2.1.png",
+                        "images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 3.1.png",
+                        "images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 4.1.png"
                 );
-
-                Texture[] meleeUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 2.1.png")),
-                        new Texture(Gdx.files.internal("images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 3.1.png")),
-                        new Texture(Gdx.files.internal("images/towers/Melee Glaive Tower/Melee Glaive Tower lvl 4.1.png"))
-                };
-
-                Weapon meleeWeapon = new Weapon(meleeTower.getPosition(), 512);
-                return new Tower(meleeTower, meleeUpgrades, meleeWeapon, 10, 4);
-                
             case "pea-shooter":
-                Sprite peaTower = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 1.png",
+                        "images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 2.png",
+                        "images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 3.png",
+                        "images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 4.png"
                 );
-
-                Texture[] peaUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 2.png")),
-                        new Texture(Gdx.files.internal("images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 3.png")),
-                        new Texture(Gdx.files.internal("images/towers/Pea shooter (Single target)/Blue Pea Shooter lvl 4.png"))
-                };
-
-                Weapon peaWeapon = new Weapon(peaTower.getPosition(), 512);
-                return new Tower(peaTower, peaUpgrades, peaWeapon, 10, 4);
-                
             case "range-powerup":
-                Sprite rangePowerup = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Powerup/Range Powerup v1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Powerup/Range Powerup v1.png",
+                        "images/towers/Powerup/Range Powerup v2.png"
                 );
-
-                Texture[] rangePowerupUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Powerup/Range Powerup v2.png"))
-                };
-
-                Weapon rangePowerupWeapon = new Weapon(rangePowerup.getPosition(), 512);
-                return new Tower(rangePowerup, rangePowerupUpgrades, rangePowerupWeapon, 10, 2);
-                
             case "strenght-powerup":
-                Sprite strenghtPowerup = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Powerup/Strength Powerup non-glowing.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0, 
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Powerup/Strength Powerup non-glowing.png",
+                        "images/towers/Powerup/Strength Powerup.png"
                 );
-
-                Texture[] strenghtPowerupUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Powerup/Strength Powerup.png"))
-                };
-                
-                Weapon strenghtPowerupWeapon = new Weapon(strenghtPowerup.getPosition(), 512);
-                return new Tower(strenghtPowerup, strenghtPowerupUpgrades, strenghtPowerupWeapon, 10, 2);
-            
-            
             case "railgun-tower":
-                Sprite railgunTower = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 1.png",
+                        "images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 2.png",
+                        "images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 3.png",
+                        "images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 4.png"
                 );
-
-                Texture[] railgunUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 2.png")),
-                        new Texture(Gdx.files.internal("images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 3.png")),
-                        new Texture(Gdx.files.internal("images/towers/Railgun Tower (Long distance sniper)/Railgun Tower lvl 4.png"))
-                };
-
-                Weapon railgunWeapon = new Weapon(railgunTower.getPosition(), 512);
-                return new Tower(railgunTower, railgunUpgrades, railgunWeapon, 10, 4);
-                
             case "rocket-tower":
-                Sprite rocketTower = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 1.png",
+                        "images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 2.png",
+                        "images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 3.png",
+                        "images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 4.png"
                 );
-
-                Texture[] rocketUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 2.png")),
-                        new Texture(Gdx.files.internal("images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 3.png")),
-                        new Texture(Gdx.files.internal("images/towers/Rocket Tower (Curve & AOE)/Orange Rocket Tower lvl 4.png"))
-                };
-
-                Weapon rocketWeapon = new Weapon(rocketTower.getPosition(), 512);
-                return new Tower(rocketTower, rocketUpgrades, rocketWeapon, 10, 4);
-                
             case "splash-tower":
-                Sprite splashTower = new Sprite(
-                        new Texture(Gdx.files.internal("images/towers/Splash Tower (AOE)/Red Splash Tower lvl 1.png")),
-                        World.GRID_CELL_SIZE,
-                        World.GRID_CELL_SIZE,
-                        0,
-                        new Vector2f(0, 0)
+                return build(
+                        "images/towers/Splash Tower (AOE)/Red Splash Tower lvl 1.png",
+                        "images/towers/Splash Tower (AOE)/Red Splash Tower lvl 2.png",
+                        "images/towers/Splash Tower (AOE)/Red Splash Tower lvl 3.png",
+                        "images/towers/Splash Tower (AOE)/Red Splash Tower lvl 4.png"
                 );
-
-                Texture[] splasUpgrades = {
-                        new Texture(Gdx.files.internal("images/towers/Splash Tower (AOE)/Red Splash Tower lvl 2.png")),
-                        new Texture(Gdx.files.internal("images/towers/Splash Tower (AOE)/Red Splash Tower lvl 3.png")),
-                        new Texture(Gdx.files.internal("images/towers/Splash Tower (AOE)/Red Splash Tower lvl 4.png")),
-                };
-
-                Weapon splashWeapon = new Weapon(splashTower.getPosition(), 512);
-                return new Tower(splashTower, splasUpgrades, splashWeapon, 10, 4);
         }
-
         return null;
+    }
+
+    private static Tower build(String... files) {
+        Texture[] textures = new Texture[files.length];
+        for (int i = 0; i < files.length; i++) {
+            textures[i] = new Texture(Gdx.files.internal(files[i]));
+        }
+        Vector2f position = new Vector2f(0, 0);
+        return new Tower(new Sprite(textures[0], World.GRID_CELL_SIZE, World.GRID_CELL_SIZE, 0, position),
+                textures, new Weapon(position, 512), 10, 4);
     }
 }
