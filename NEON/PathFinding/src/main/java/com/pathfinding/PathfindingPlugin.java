3e25048ec4e3f5d8aae8f05967e989b788adb834
danielvs1234 <danielvs@live.dk>
2018-04-05 11:20:50 +0200

    Pathfinding component added

---
+++ NEON/PathFinding/src/main/java/com/pathfinding/PathfindingPlugin.java
@@ -0,0 +1,26 @@
+package com.pathfinding;
+
+import com.neon.libary.GameData;
+import com.neon.libary.World;
+import com.neon.libary.interfaces.IPathFindingService;
+import com.neon.libary.interfaces.Plugin;
+
+public class PathfindingPlugin implements Plugin {
+    private GameData gameData;
+    private World world;
+
+    public PathfindingPlugin(GameData gameData, World world) {
+        this.gameData = gameData;
+        this.world = world;
+    }
+
+    @Override
+    public void start() {
+        gameData.addService(IPathFindingService.class, new PathFinder(world));
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
