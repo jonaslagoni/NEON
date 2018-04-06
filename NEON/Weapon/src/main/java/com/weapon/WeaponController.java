ddd13e5d86fe571bf62345305e958548fecb4321
mathn16 <mathias626@hotmail.com>
2018-04-06 12:21:28 +0200

    Dis sum gud shit mon

--- NEON/Weapon/src/main/java/com/weapon/WeaponController.java
+++ NEON/Weapon/src/main/java/com/weapon/WeaponController.java
@@ -12,19 +12,11 @@ import static com.library.vectors.VectorUtils.distanceSquare;
 
 class WeaponController implements Controller {
 
-    private final World world;
-    private final ICollisionService collisionService;
-    private final IProjectileService projectileService;
-    private final ITargetingService targetingService;
+    private World world;
+    private ICollisionService collisionService = null;
+    private IProjectileService projectileService = null;
+    private ITargetingService targetingService = null;
 
-    WeaponController(World world,
-                     ICollisionService collisionService,
-                     IProjectileService projectileService, ITargetingService targetingService) {
-        this.world = world;
-        this.collisionService = collisionService;
-        this.projectileService = projectileService;
-        this.targetingService = targetingService;
-    }
 
     private static boolean isCloser(Vector2f source, Vector2f first, Vector2f second) {
         return distanceSquare(first, source) < distanceSquare(first, second);
@@ -62,6 +54,30 @@ class WeaponController implements Controller {
     private Vector2f findTarget(Drawable targetSprite, Weapon weapon) {
         return targetingService.calculateTargetVector(weapon.position, targetSprite.getSprite());
 
+    }
+    
+    public void setCollisionService(ICollisionService ics){
+        this.collisionService = ics;
+    }
+    
+    public void removeCollisionService(){
+        this.collisionService = null;
+    }
+    
+    public void setProjectileService(IProjectileService ips){
+        this.projectileService = ips;
+    }
+    
+    public void removeProjectileService(){
+        this.projectileService = null;
+    }
+    
+    public void setTargetingService(ITargetingService its){
+        this.targetingService = its;
+    }
+    
+    public void removeTargetingService(){
+        this.targetingService = null;
     }
 }
     
--- NEON/Weapon/src/main/java/com/weapon/WeaponPlugin.java
+++ NEON/Weapon/src/main/java/com/weapon/WeaponPlugin.java
@@ -2,27 +2,19 @@ package com.weapon;
 
 import com.library.GameData;
 import com.library.World;
-import com.library.interfaces.ICollisionService;
-import com.library.interfaces.IProjectileService;
-import com.library.interfaces.ITargetingService;
 import com.library.interfaces.Plugin;
 
 public class WeaponPlugin implements Plugin {
 
-    private GameData gameData;
     private World world;
 
     public WeaponPlugin(World world, GameData gameData) {
         this.world = world;
-        this.gameData = gameData;
     }
 
     @Override
     public void start() {
-        gameData.addController(new WeaponController(world,
-                gameData.getService(ICollisionService.class),
-                gameData.getService(IProjectileService.class),
-                gameData.getService(ITargetingService.class)));
+        
     }
 
     @Override
